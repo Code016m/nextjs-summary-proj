c617d658fe1a61e6541f3d3ade2be83fb2160657
Code016m <miguel.r.araujo16@gmail.com>
2026-08-13 19:11:10 -0300

    Add MeetupDetail component and implement routing in MeetupItem

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,14 @@
+import MeetupDetail from "../../components/meetups/MeetupDetail";
+
+function MeetupDetails() {
+  return (
+    <MeetupDetail
+      image="https://upload.wikimedia.org/wikipedia/commons/a/aa/Marienplatz_Munich.jpn.png?utm_source=commons.wikimedia.org&utm_campaign=index&utm_content=original"
+      title="First Meetup"
+      address="Some Street 5, Some City"
+      description="This is a first meetup"
+    />
+  );
+}
+
+export default MeetupDetails;
